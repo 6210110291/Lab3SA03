54a3b59ba796491d10ef1916bc535382cddbdbf2
6210110291 <6210110291@psu.ac.th>
2021-07-11 15:27:19 +0700

    Part7-2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
+import CharacterCard from './CharacterCard';
 import './App.css';
 
-import WordCard from './WordCard';
-import CharacterCard from './CharacterCard';
-const word = "Hello"  
-function App() {
-    return (
-      <div>
-          <WordCard value={word}/>
-      </div>
-  );
-}
+const imagesArray = [
+  {
+    type: "img1",
+    image: require('https://drive.google.com/file/d/1EpdyAOPTgkkEqkOWGOPjcpkls-DxLHW9/view?usp=sharing')
+  },
+  {
+    type: "img2",
+    image: require('https://drive.google.com/file/d/1_3giSMWXm_Bkmq0UdDm8XfaSJ7Dq0b10/view?usp=sharing')
+  },
+  {
+    type: "img3",
+    image: require('https://drive.google.com/file/d/1M_HaWRuoLFWPc3ivr_NiNHnJ2mg3rPJr/view?usp=sharing')
+  },
+  {
+    type: "img4",
+    image: require('https://drive.google.com/file/d/1CyhuK4SBSGzgPerGppdxiZIVFpez-qDB/view?usp=sharing')
+  },
+  {
+    type: "img5",
+    image: require('https://drive.google.com/file/d/1M_HaWRuoLFWPc3ivr_NiNHnJ2mg3rPJr/view?usp=sharing')
+  },
+  {
+    type: "img6",
+    image: require('https://drive.google.com/file/d/17QBHjkFX6ZLu20qZi-qfM6bB1eK3_y4l/view?usp=sharing')
+  }
+];
+
+
 
 export default App;
