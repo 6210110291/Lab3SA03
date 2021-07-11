90e0694d0dbff3c8921bb1b040c31fc14b3c0a3b
6210110291 <6210110291@psu.ac.th>
2021-07-11 16:37:06 +0700

    Part7-4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React, {useEffect, useState, useRef} from 'react';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  Button,
+  DialogTitle
+} from "@material-ui/core";
 import CharacterCard from './CharacterCard';
 import './App.css';
 
@@ -45,6 +53,87 @@ export default function App() {
   const [cards, setCards]=useState (
     shuffleCards.bind(null, imagesArray.concat(imagesArray))
   );
+  const [openCards, setOpenCards] = useState([]);
+  const [clearedCards, setClearedCards] = useState({});
+  const [shouldDisableAllCards, setShouldDisableAllCards] = useState(false);
+  const [moves, setMoves] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [bestScore, setBestScore] = useState(
+    JSON.parse(localStorage.getItem("bestScore")) || Number.POSITIVE_INFINITY
+  );
+  const timeout = useRef(null);
+
+  const disable = () => {
+    setShouldDisableAllCards(true);
+  };
+  const enable = () => {
+    setShouldDisableAllCards(false);
+  };
+
+  const checkCompletion = () => {
+    if (Object.keys(clearedCards).length === uniqueElementsArray.length) {
+      setShowModal(true);
+      const highScore = Math.min(moves, bestScore);
+      setBestScore(highScore);
+      localStorage.setItem("bestScore", highScore);
+    }
+  };
+
+  const evaluate = () => {
+    const [first, second] = openCards;
+    enable();
+    if (cards[first].type === cards[second].type) {
+      setClearedCards((prev) => ({ ...prev, [cards[first].type]: true }));
+      setOpenCards([]);
+      return;
+    }
+    // This is to flip the cards back after 500ms duration
+    timeout.current = setTimeout(() => {
+      setOpenCards([]);
+    }, 500);
+  };
+
+  const handleCardClick = (index) => {
+    if (openCards.length === 1) {
+      setOpenCards((prev) => [...prev, index]);
+      setMoves((moves) => moves + 1);
+      disable();
+    } else {
+      clearTimeout(timeout.current);
+      setOpenCards([index]);
+    }
+  };
+
+  useEffect(() => {
+    let timeout = null;
+    if (openCards.length === 2) {
+      timeout = setTimeout(evaluate, 300);
+    }
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [openCards]);
+
+  useEffect(() => {
+    checkCompletion();
+  }, [clearedCards]);
+  const checkIsFlipped = (index) => {
+    return openCards.includes(index);
+  };
+
+  const checkIsInactive = (card) => {
+    return Boolean(clearedCards[card.type]);
+  };
+
+  const handleRestart = () => {
+    setClearedCards({});
+    setOpenCards([]);
+    setShowModal(false);
+    setMoves(0);
+    setShouldDisableAllCards(false);
+    // set a shuffled deck of cards
+    setCards(shuffleCards(uniqueElementsArray.concat(uniqueElementsArray)));
+  };
 
 }
 
@@ -81,6 +170,35 @@ return (
           </div>
         )}
       </div>
+
+      <div className = "Restart">
+        <Buttom onClock={handleReStart} color="primary" variant="contained">
+          Restart
+        </Buttom>
+      </div>
     </footer>
+
+    <Dialog open = {showModal}
+    disableBackdropClick
+    disableEscapeKwyDown
+    aria-labelledby = "alert-dialog-title"
+    aria-describeadby = "alert-dialog-description"
+    >
+      <DialogTitle id = "alert-dialog-title">
+        Yeah!! You win!
+      </DialogTitle>
+
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          You completed the game in {moves} moves. Your best score is{" "} {bestScore} moves.
+        </DialogContentText>
+      </DialogContent>
+
+      <DialogActions>
+        <Button onClick={handleRestart} color="primary">
+          Restart
+        </Button>
+        </DialogActions>
+    </Dialog>
   </div>
 )
